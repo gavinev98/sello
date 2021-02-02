803a0f4b88e3f39f2dcbce4d115e9ce15cabf26b
gavinev98 <gavin.everett48@hotmail.com>
2021-02-02 17:14:56 +0000

    added image property to products.

--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -9,15 +9,16 @@ const producto = [
         id: 1,
         name : "Shoes",
         desc : "running shoes",
-        price: "£ 100"
+        price: "£ 100",
+        image: 'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
     },
 
     {
         id: 1,
         name : "Macbook Pro",
         desc : "a computer for all needs",
-        price: "£ 2000"
-    }
+        price: "£ 2000",
+        image: 'https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'  }
 ]
 
 
