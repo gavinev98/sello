20652ca98b61f7f3a267cc0f8f9daf3a9c49269c
gavinev98 <gavin.everett48@hotmail.com>
2021-02-06 11:26:22 +0000

    creating api call to retrieve products.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 import { Products, Navbar } from './components';
 
 const App = () => {
+
+    //creating a hook for products.
+  const [products, setProducts] = useState([]);
+
+  //fetching products onload of app.
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    //setting products equal to data returned from commerce api call.
+    setProducts(data);
+  }
+
+
   return (
-    <div>
+    <div> 
       <Navbar />
       <Products />
     </div>
