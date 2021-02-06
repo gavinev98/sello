ea38753cf611ad29bba94552c67d9b52713fc047
gavinev98 <gavin.everett48@hotmail.com>
2021-02-06 12:11:11 +0000

    items retrieved from store.

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -8,25 +8,22 @@ import useStyles from './styles';
 const Product = ({product}) => {
 
     const classes = useStyles();
-
-    console.log(product);
-
-    return <div>test</div>
+  
 
     return (
         <Card className={classes.root}>
-        <CardMedia className={classes.media} image={product.image} title={product.name} />
+        <CardMedia className={classes.media} image={product.media.source} title={product.name} />
         <CardContent>
             <div className={classes.cardContent}>
             <Typography variant="h5" gutterBottom>
              {product.name}
             </Typography>
             <Typography variant="h4">
-             {product.price}
+             {product.price.formatted_with_symbol}
             </Typography>
             </div>
             <Typography variant="body2" color="textSecondary">
-             {product.desc}
+             {product.description}
             </Typography>
         </CardContent>
 
