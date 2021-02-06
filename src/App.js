113ae01368985e67086d584d6c5ba895cad7da4f
gavinev98 <gavin.everett48@hotmail.com>
2021-02-06 15:12:36 +0000

    fetching the current state of  the cart.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import { Products, Navbar } from './components';
 
 const App = () => {
 
-    //creating a hook for products.
+    //creating a state/hook for products.
   const [products, setProducts] = useState([]);
+
+  //creating a state/hook for the shopping cart.
+  const [cart, setCart] = useState({});
+
 
   //fetching products onload of app.
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     //setting products equal to data returned from commerce api call.
     setProducts(data);
+  }
+
+  //fetching the current cart session if there is one avaliable.
+  const fetchCart = async () => {
+    //storing the response in a variable.
+    const response = await commerce.cart.retrieve();
+
+    //setting the carts state if there is any.
+    setCart(response);
+
+
   }
 
   //similar to onload function runs at the start.
