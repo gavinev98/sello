fae1b956c3f5e66a0f9957caeb69c096a15d02d7
gavinev98 <gavin.everett48@hotmail.com>
2021-02-07 11:32:26 +0000

    creating function to check if cart is empty or not

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
-
+import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 
 
 const Cart = () => {
+    //check if shopping cart is empty.
+    const isEmpty = true;  
+    
+    
+
+    //a component to display if the cart is currently empty.
+    const EmptyCart = () => {
+        <Typography variant="subtitle1">You have no items in your shopping cart !</Typography>
+    }
+
+    //a component to display is the cart is filled.
+    const FilledCart = () => {
+
+    }
+
     return (
-        <div>
-            
-        </div>
+            <Container>
+                <div className={classes.toolbar} />
+
+                <Typography className={classes.title} variant="h3" >Your Shopping Cart!</Typography>
+                {isEmpty ? <EmptyCart /> : <FilledCart />}
+            </Container>
     );
 };
 
