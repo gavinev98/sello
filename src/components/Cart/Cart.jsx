bbd3bcbc81096ecb7e2cc753283c1b9a74b67da1
gavinev98 <gavin.everett48@hotmail.com>
2021-02-07 11:53:53 +0000

    creating display for all cart items.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
+import useStyles from './styles';
 
 
-
-const Cart = () => {
+const Cart = ({ cart }) => {
+    //import styles
+    const classes = useStyles();
+      
     //check if shopping cart is empty.
-    const isEmpty = true;  
-    
-    
+    const isEmpty = !cart.line_items.length;
 
+
+    
     //a component to display if the cart is currently empty.
     const EmptyCart = () => {
         <Typography variant="subtitle1">You have no items in your shopping cart !</Typography>
@@ -16,7 +19,15 @@ const Cart = () => {
 
     //a component to display is the cart is filled.
     const FilledCart = () => {
-
+        	<>
+            <Grid container spacing={3}>
+            {cart.line_items.map(lineItem => (
+            <Grid item xs={12} sm={4} key={lineItem.id}>
+            <CartItem />
+            </Grid>
+            ))}
+            </Grid>
+         </>
     }
 
     return (
