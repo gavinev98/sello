a86ea2d1a94727558a81f1330e08776e895f121e
gavinev98 <gavin.everett48@hotmail.com>
2021-02-10 18:40:46 +0000

    setting up different route paths.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 
 const App = () => {
 
@@ -47,11 +48,19 @@ const App = () => {
   console.log(cart);
 
   return (
+    <Router>
     <div> 
       <Navbar totalItems={cart.total_items} />
-     { /* <Products products={products} onAddToCart={handleAddToCart} /> */}
-     <Cart cart={cart} />
+      <Switch>
+        <Route exact path="/">
+        <Products products={products} onAddToCart={handleAddToCart} />
+        </Route>
+        <Route exact path="/cart">
+        <Cart cart={cart} />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 };
 
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -18,7 +18,12 @@ const CartItem = ({ item }) => {
                 <Typography variant="h5">{item.line_total.formatted_with_symbol}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                
+                <div className={classes.buttons}>
+                    <Button type="button" size="small"> - </Button>
+                    <Typography>{item.quantity}</Typography>
+                    <Button type="button" size="small"> + </Button>
+                </div>
+                <Button variant="contained" type="button" color="secondary">Remove</Button>
             </CardActions>
         </Card>
     );
