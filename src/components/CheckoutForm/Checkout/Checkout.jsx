5800407f1a134a50889ff568cc4cab26daf8f608
gavinev98 <gavin.everett48@hotmail.com>
2021-02-13 11:07:33 +0000

    Using stepper component to create fluid steps.

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core';
+import classes from '*.module.css';
 
+const steps = ['Shipping Address', 'Payment Details'];
 
 const Checkout = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+          <div className={classes.toolbar}  />
+            <main className={classes.layout}>
+            <Paper className={classes.paper}>
+            <Typography variant="h4" align="center">Checkout</Typography>
+            <Stepper activeStep={0} className={classes.stepper}>
+            {steps.map((step) => (
+                <Step key={step}>
+                    <StepLabel>{step}</StepLabel>
+                </Step>
+            ))}
+            </Stepper>
+            </Paper>
+            </main>
+
+        </>
     );
 };
 
