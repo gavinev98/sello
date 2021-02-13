a6d0bc0e3f905bd0d7caf9f557f390a552acbafa
gavinev98 <gavin.everett48@hotmail.com>
2021-02-13 11:31:18 +0000

    rendering forms depending on stepper.

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core';
 import useStyles from './styles.js';
 
@@ -6,25 +6,35 @@ const steps = ['Shipping Address', 'Payment Details'];
 
 const Checkout = () => {
 
+    //setting and retrieving the active step.
+    const [activeStep, setActiveStep] = useState(0);
+
     const classes = useStyles();
 
+    const form  = () => activeStep == 0 
+        ? <AddressForm />
+        : <PaymentForm />
+
+    
 
     return (
         <>
           <div className={classes.toolbar}  />
             <main className={classes.layout}>
-            <Paper className={classes.paper}>
+        <Paper className={classes.paper}>
             <Typography variant="h4" align="center">Checkout</Typography>
-            <Stepper activeStep={0} className={classes.stepper}>
+            <Stepper activeStep={activeStep} className={classes.stepper}>
             {steps.map((step) => (
                 <Step key={step}>
                     <StepLabel>{step}</StepLabel>
                 </Step>
             ))}
             </Stepper>
-            </Paper>
-            </main>
 
+
+        </Paper>
+            </main>
+            {activeStep === steps.length ? <Confirmation /> : <Form />}
         </>
     );
 };
