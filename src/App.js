d171f7e7e913ec3135a02f09ab40c4306a84fc7e
gavinev98 <gavin.everett48@hotmail.com>
2021-02-13 10:59:42 +0000

    importing necessary items from material UI.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
-import { Products, Navbar, Cart } from './components';
+import { Products, Navbar, Cart, Checkout } from './components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 
 const App = () => {
