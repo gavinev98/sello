43cc042e4fe6fef2d1773c3406399bbaa56e9e77
gavinev98 <gavin.everett48@hotmail.com>
2021-02-14 14:36:45 +0000

    passing cart to checkout to generate at token.

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ const App = () => {
           />
         </Route>
         <Route exact path="/checkout">
-         <Checkout />
+         <Checkout cart={cart} />
         </Route>
       </Switch>
     </div>
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core';
 import useStyles from './styles.js';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
-
+import { commerce } from '../../../lib/commerce';
 const steps = ['Shipping Address', 'Payment Details'];
 
-const Checkout = () => {
+const Checkout = ({ cart }) => {
 
     //setting and retrieving the active step.
     const [activeStep, setActiveStep] = useState(0);
 
     const classes = useStyles();
+
+    useEffect(() => {
+        const generateToken = async () => {
+            try{
+                const token = commerce.checkout.generateToken(cart.id, { type: 'cart'});
+            } catch (error) {
+
+            }
+        }
+    }, []);
 
     const Form  = () => activeStep == 0 
         ? <AddressForm />
