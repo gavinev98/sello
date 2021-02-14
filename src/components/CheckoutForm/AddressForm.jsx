173ee7dad49c666dda961381a45f7fa32150533e
gavinev98 <gavin.everett48@hotmail.com>
2021-02-14 11:10:20 +0000

    setting up custom text field for add + payment.

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -12,6 +12,14 @@ const AddressForm = () => {
     return (
         <>
            <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit={}>
+                <Grid container spacing={3}>
+                
+
+                </Grid>
+                </form>
+            </FormProvider>
         </>
     );
 };
