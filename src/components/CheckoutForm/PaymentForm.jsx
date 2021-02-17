fa97abdf38afcb048e086fb3c26b9f9ffb256b47
gavinev98 <gavin.everett48@hotmail.com>
2021-02-17 15:05:15 +0000

    setting up payment form with stripe js.

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementsConsumer} from '@stripe/react-stripe-js';
 
 
 
