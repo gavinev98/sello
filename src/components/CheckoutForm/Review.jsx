3b17d81bd51f70ab3742870035d466bc276a6684
gavinev98 <gavin.everett48@hotmail.com>
2021-02-18 15:27:17 +0000

    looping over all products in the checkout token.

--- src/components/CheckoutForm/Review.jsx
+++ src/components/CheckoutForm/Review.jsx
@@ -6,7 +6,12 @@ const Review = ({ checkoutToken }) => {
         <>
         <Typography variant="h6" gutterBottom>Order Summary</Typography>
         <List disablePadding>
+            {checkoutToken.live.line_items.map(product) => (
+                <ListItem style={{padding : '10px 0'}} key={product.name}>
 
+                    
+                </ListItem>
+            )}
         </List>
         </>
     );
