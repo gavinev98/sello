34f9a069258b39dc189f88b0e2cd57692d13e872
gavinev98 <gavin.everett48@hotmail.com>
2021-02-18 15:54:31 +0000

    implementing stripe payment method.

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -4,10 +4,28 @@ import { Elements, CardElement, ElementsConsumer} from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js';
 import Review from './Review';
 
+
+const stripePromise = loadStripe('..');
+
 const PaymentForm = ({ shippingData, checkoutToken }) => {
+
+
+
     return (
         <>
          <Review checkoutToken={checkoutToken} />
+         <Divider />
+         <Typography variant="h6" gutterBottom style={{margin: '20px 0'}}>Payment Method</Typography>
+
+         <Elements stripe={stripePromise}>
+            <ElementsConsumer>
+                {({elements, stripe}) => (
+                    <form>
+                        
+                    </form>
+                )}
+            </ElementsConsumer>
+         </Elements>
         </>
     );
 };
