2d9da7fb778d5624a7f36ef56ff1764c1893442e
gavinev98 <gavin.everett48@hotmail.com>
2021-02-20 11:07:40 +0000

    setting up orderData to be sumitted to commerce js

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -12,8 +12,39 @@ const PaymentForm = ({ shippingData, checkoutToken, backStep }) => {
     const handleSubmit = (event, elements, stripe) => {
         //this is to prevent the website from re loading.
         event.preventDefault();
+        
+        //quick error checking to see if there is no stripe or no elements.
+        if(!stripe || !elements) return;
 
+        //storing everything inside of card element
+        const cardElement = elements.getElement(CardElement);
 
+        const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 'card', card : cardElement});
+
+        if(error) {
+            console.log(error);
+        } else {
+            //final object to contain all of the data eg everythign inside of our cart.
+            const orderData = {
+                list_items : checkoutToken.live.line_items,
+                customer: { firstname: shippingData.firstName, lastname: shippingData.lastName, email: shippingData.email},
+                shipping: { name: 'Primary',
+                 street: shippingData.address1,
+                 town_city: shippingData.city,
+                 county_state: shippingData.shippingSubdivision,
+                 postal_zip_code: shippingData.zip,
+                 country: shippingData.shippingCountry
+                 },
+                 fulfillment: { shipping_method: shippingData.shippingOption },
+                 payment : {
+                     gateway: 'stripe',
+                     stripe: {
+                         payment_method_id: paymentMethod.id
+                     }
+                 }
+            }
+
+        }
 
     }
 
